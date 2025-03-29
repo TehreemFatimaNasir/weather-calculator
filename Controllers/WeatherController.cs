78ae6bfae8c49234f9ef74633ed89c31252b9008
tehreem fatima <tehism672@gmail.com>
2025-03-29 23:27:41 +0500

    message

---
+++ Controllers/WeatherController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace weather.Controllers
+{
+    public class WeatherController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+           public IActionResult Calculate(double temperature, string scale)
+             {
+            double convertedTemperature = 0;
+            string convertedScale = "";
+
+            if (scale == "Celsius")
+            {
+                convertedTemperature = (temperature * 9 / 5) + 32;
+                convertedScale = "Fahrenheit";
+            }
+            else if (scale == "Fahrenheit")
+            {
+                convertedTemperature = (temperature - 32) * 5 / 9;
+                convertedScale = "Celsius";
+            }
+
+            ViewBag.ConvertedTemperature = convertedTemperature;
+            ViewBag.ConvertedScale = convertedScale;
+
+            return View("Calculate");
+        }
+    }
+}
---
+++ Models/WeatherModel.cs
@@ -0,0 +1,10 @@
+﻿namespace weather.Models
+{
+    public class WeatherModel
+    {
+        public double Temperature { get; set; }
+        public string Scale { get; set; }  
+        public double ConvertedTemperature { get; set; }
+        public string ConvertedScale { get; set; }
+    }
+}
